baadef54241fa5dd981bf13574bc66918a34541d
UberPyro <uberpyro181@gmail.com>
2017-08-31 22:30:14 -0400

    Remove bad fix

--- MakeMyTeam/settings.py
+++ MakeMyTeam/settings.py
@@ -142,7 +142,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, '../mkmt/static'),
-)
+STATIC_URL = '/static/'
