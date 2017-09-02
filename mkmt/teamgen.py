87d65f8ad0213c7b143f3428ddd99c97337ebd3d
UberPyro <uberpyro181@gmail.com>
2017-09-02 10:14:42 -0400

    Fix z-move filter
    z-move filter was checking the species list rather than the species + set list

--- mkmt/teamgen.py
+++ mkmt/teamgen.py
@@ -145,11 +145,12 @@ def gen_team_brute(emons, ddir, dweight, genq=1, excl=set(), sus_array=sus_array
 def mmsfilt(team_data):  # multiple mega and species filter; also now filters z-moves
     return tuple(team_data[part][idx] for idx in (np.array([team_idx
     for team_idx in xrange(team_data[0].shape[0])
-    for pkmn_spcs in ([aps_list_full[pkmn].split("|")[0] for pkmn in team_data[0][team_idx].tolist()],)
+    for team in (team_data[0][team_idx].tolist(),)
+    for pkmn_spcs in ([aps_list_full[pkmn].split("|")[0] for pkmn in team,)
     if (
     len(pkmn_spcs) == len(set(pkmn_spcs)) and
     len(filter(lambda pkmn: "-Mega" in pkmn, pkmn_spcs)) <= 1 and
-    len(filter(lambda pkmn: "z-" in pkmn or "Z DD" in pkmn, pkmn_spcs)) <= 1
+    len(filter(lambda pkmn: "z-" in pkmn or "Z DD" in pkmn, team)) <= 1
     )
     ]),)
     for part in xrange(2))
